c681969bdddf193b8ef3ace631bd9f8dc7babfd0
Toby Hsieh <thrasher13b@gmail.com>
2024-09-01 23:07:11 -0700

    Migrate Gradle scripts to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "io.github.tobyhs.usbdebugstatus"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "io.github.tobyhs.usbdebugstatus"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+        getByName("debug") {
+            versionNameSuffix = "-debug"
+        }
+
+        create("local") {
+            initWith(getByName("release"))
+            signingConfig = signingConfigs.getByName("debug")
+            versionNameSuffix = "-local"
+        }
+
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    testImplementation("androidx.test:core:1.6.1")
+    testImplementation("androidx.test.ext:junit:1.2.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.mockito:mockito-core:4.6.1")
+    testImplementation("org.robolectric:robolectric:4.11.1")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,4 +14,4 @@ dependencyResolutionManagement {
     }
 }
 
-include ':app'
+include(":app")
