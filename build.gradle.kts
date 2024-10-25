779cd9c03be507935e39a14171f312f87c01f37e
Toby Hsieh <thrasher13b@gmail.com>
2024-10-25 14:01:27 -0700

    Enable Gradle dependency locking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+allprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.7.1" apply false
 }
